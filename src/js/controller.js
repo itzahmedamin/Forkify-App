5b0ac6d009c6a26624605fb369aedfc242aa7655
itzahmedamin <aa5334497@gmail.com>
2025-08-11 12:43:57 +0300

    fix

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,10 +11,6 @@ import bookmarksView from './views/bookmarkView.js';
 import addRecipeView from './views/addRecipeView.js';
 import { MODAL_CLOSE_SEC } from './config.js';
 
-// if (module.hot) {
-//   module.hot.accept();
-// }
-
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
